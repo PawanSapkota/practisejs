9de64144d8aa2b1b45ac20392f376dcc68c133b7
pawan <uiintern.infoxit@gmail.com>
2024-10-17 16:14:09 +0545

    solve sum question

--- qn/twosum.js
+++ qn/twosum.js
@@ -33,16 +33,22 @@ console.log("two sum");
 // Output: [0,1]
 
 var twoSum = function(nums, target) {
-    console.log("nums")
-    let map = new Map();
-    console.log("map",map)
-   
-
-       
-
+    const map = new Map();
     
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        
+        if (map.has(complement)) {
+            return [map.get(complement), i]; 
+        }
+        
+        map.set(nums[i], i);
+    }
+    
+    return null; 
 };
 
-console.log(twoSum([2,7,11,15],9))
+console.log(twoSum([2, 7, 11, 15], 27)); 
+
 
 
